6e4394d00939721f9c15fc2787e303741b04aa11
nbogie <neillbogie@googlemail.com>
2023-12-08 14:28:18 +0000

    simplified example, but add second .js file

---
+++ myPalettes.js
@@ -0,0 +1,11 @@
+/** @return a palette chosen at random,
+ * as an array of strings each of which represents a colour (e.g. "#f04155")
+ */
+function randomPalette() {
+    const allPalettes = [
+        ["#f04155", "#ff823a", "#f2f26f", "#fff7bd", "#95cfb7"],
+        ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"],
+        ["#aaff00", "#ffaa00", "#ff00aa", "#aa00ff", "#00aaff"],
+    ];
+    return random(allPalettes);
+}
--- mySketch.js
+++ mySketch.js
@@ -1,65 +1,27 @@
-let walkerX;
-let walkerY;
-
-let palette;
-
-const allPalettes = [
-  ["#f04155", "#ff823a", "#f2f26f", "#fff7bd", "#95cfb7"],
-  ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"],
-  ["#aaff00", "#ffaa00", "#ff00aa", "#aa00ff", "#00aaff"],
-];
+//see also myPalettes.js
+let currentPalette;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // frameRate(20);
-  restartWalker();
+    createCanvas(windowWidth, windowHeight / 2);
+    currentPalette = randomPalette();
+    noStroke();
+    background("white");
 }
 
 function draw() {
-  drawWalker();
-  moveWalker();
-}
-
-function restartWalker() {
-  walkerX = width / 2;
-  walkerY = height / 2;
-  background("linen");
-  palette = random(allPalettes);
-}
-
-function drawWalker() {
-  let diam = random(10, 100);
-
-  fill(random(palette));
-  noStroke();
-  rectMode(CENTER);
-
-  square(walkerX, walkerY, diam, random(0, 200));
-
-  fill(random(palette));
-  square(walkerX, walkerY, diam / 2);
-}
-
-function moveWalker() {
-  const step = 50;
-  walkerX += random(-step, step);
-  walkerY += random(-step, step);
-  //if circle pos is off canvas then recentre pos and wipe
-  if (walkerX > width || walkerX < 0 || walkerY > height || walkerY < 0) {
-    restartWalker();
-  }
+    fill(random(currentPalette));
+    const x = random(0, width);
+    const y = random(0, height);
+    circle(x, y, 100);
 }
 
 function mouseClicked() {
-  walkerX = mouseX;
-  walkerY = mouseY;
+    background(255);
+    currentPalette = randomPalette();
 }
 
 function keyPressed() {
-  if (key === "s") {
-    save("sunset");
-  }
-  if (key === "w") {
-    background("linen");
-  }
+    if (key === "s") {
+        save("my-p5-screenshot");
+    }
 }
