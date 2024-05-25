8a47175a596e009a4ab629f02ed807ef684e0f4c
nbogie <neillbogie@googlemail.com>
2024-05-25 09:39:41 +0100

    initial

--- js/myPalettes.js
+++ js/myPalettes.js
@@ -1,20 +1,118 @@
 //This is a second javascript file containing functions to support the main one, "mySketch.js"
 //It must also be loaded in index.html
 
-/** @returns {string[]} a palette chosen at random,
+/** @typedef {{
+        colors: string[],
+        name: string,
+        background?: string,
+        stroke?:string,
+        size: number
+    }} Palette */
+/** @returns {Palette} a palette chosen at random,
  * as an array of strings each of which represents a colour (e.g. "#f04155")
  */
 function randomPalette() {
+    /** @type {Palette[]} */
     const allPalettes = [
-        [
-            "#69766f",
-            "#9ed6cb",
-            "#f7e5cc",
-            "#9d8f7f",
-            "#936454",
-            "#bf5c32",
-            "#efad57",
-        ],
+        {
+            name: "nowak",
+            colors: [
+                "#e85b30",
+                "#ef9e28",
+                "#c6ac71",
+                "#e0c191",
+                "#3f6279",
+                "#ee854e",
+                "#180305",
+            ],
+            stroke: "#180305",
+            background: "#ede4cb",
+            size: 7,
+        },
+        {
+            name: "tundra3",
+            colors: [
+                "#87c3ca",
+                "#7b7377",
+                "#b2475d",
+                "#7d3e3e",
+                "#eb7f64",
+                "#d9c67a",
+                "#f3f2f2",
+            ],
+            size: 7,
+        },
+        {
+            name: "jupiter",
+            colors: [
+                "#c03a53",
+                "#edd09e",
+                "#aab5af",
+                "#023629",
+                "#eba735",
+                "#8e9380",
+                "#6c4127",
+            ],
+            stroke: "#12110f",
+            background: "#e6e2d6",
+            size: 7,
+        },
+        {
+            name: "retro",
+            colors: [
+                "#69766f",
+                "#9ed6cb",
+                "#f7e5cc",
+                "#9d8f7f",
+                "#936454",
+                "#bf5c32",
+                "#efad57",
+            ],
+            size: 7,
+        },
+        {
+            name: "tsu_akasaka",
+            colors: [
+                "#687f72",
+                "#cc7d6c",
+                "#dec36f",
+                "#dec7af",
+                "#ad8470",
+                "#424637",
+            ],
+            stroke: "#251c12",
+            background: "#cfc7b9",
+            size: 6,
+        },
+
+        {
+            name: "tundra2",
+            colors: [
+                "#5f9e93",
+                "#3d3638",
+                "#733632",
+                "#b66239",
+                "#b0a1a4",
+                "#e3dad2",
+            ],
+            size: 6,
+        },
+        {
+            name: "tsu_arcade",
+            colors: [
+                "#4aad8b",
+                "#e15147",
+                "#f3b551",
+                "#cec8b8",
+                "#d1af84",
+                "#544e47",
+            ],
+            stroke: "#251c12",
+            background: "#cfc7b9",
+            size: 6,
+        },
     ];
-    return random(allPalettes);
+
+    const p = random(allPalettes);
+    return { ...p, colors: shuffle(p.colors) };
 }
--- js/mySketch.js
+++ js/mySketch.js
@@ -1,28 +1,158 @@
+//@ts-check
 //This is our main javascript file constituting our p5.js sketch.
 //It must be loaded from index.html
 //It assumes that the file "myPalettes.js" has also been loaded
 
+/** @type {Palette} */
 let currentPalette;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    currentPalette = randomPalette();
     noStroke();
-    background(currentPalette[2]);
+    restart();
+    noLoop();
+}
+
+/**
+ *
+ * @param {Palette} palette
+ * @returns { {bgColour:string, otherColours: string[]}}
+ */
+function splitPalette(palette) {
+    if (palette.background) {
+        const otherColours = palette.colors.filter(
+            (c) => c !== palette.background
+        );
+        return { bgColour: palette.background, otherColours };
+    }
+
+    const [bgColour, ...otherColours] = currentPalette.colors;
+    return { bgColour, otherColours };
 }
 
 function draw() {
-    fill(random(currentPalette));
-    // circle(mouseX, mouseY, 100);
+    const { bgColour, otherColours } = splitPalette(currentPalette);
+    background(bgColour);
+    drawGrid(otherColours);
+    textSize(30);
+    textAlign(RIGHT, CENTER);
+    text(currentPalette.name, width - 30, height - 30);
 }
 
-function mouseClicked() {
-    background(255);
+function restart() {
     currentPalette = randomPalette();
+    redraw();
+}
+function drawGrid(palette) {
+    const cellSize = 150;
+    let counter = 0;
+
+    for (let y = 0; y <= height; y += cellSize) {
+        for (let x = 0; x <= width; x += cellSize) {
+            drawItem(x, y, cellSize * 0.66, shuffle(palette));
+            counter++;
+        }
+    }
+}
+function mouseClicked() {
+    restart();
 }
 
 function keyPressed() {
     if (key === "s") {
         save("my-p5-screenshot");
+    }
+}
+function drawItem(x, y, cellSize, palette) {
+    push();
+    translate(x, y);
+
+    drawRings(cellSize, palette);
+    drawPetals(cellSize, palette);
+    pop();
+}
+
+function drawPetals(cellSize, palette) {
+    const numPetals = random([6, 12]);
+    const [c1, c2, c3, c4, c5, c6, c7] = palette;
+    fill(c1);
+    const petalFunction = random([
+        (sz) => circle(0, 0, sz),
+        (sz) => {
+            stroke(c2);
+            strokeWeight(cellSize / 10);
+            line(0, 0, sz, 0);
+        },
+        (sz) => {
+            rectMode(CENTER);
+            strokeWeight(cellSize / 10);
+            stroke(c3);
+            rect(-sz * 0.5, 0, sz * 4, 0);
+        },
+    ]);
+    for (let angle = 0; angle < TWO_PI; angle += TWO_PI / numPetals) {
+        push();
+        rotate(angle);
+        translate(cellSize * 0.3, 0);
+        petalFunction(cellSize / 12);
+
+        pop();
+    }
+}
+function drawRings(cellSize, palette) {
+    if (random() < 0.1) {
+        return;
+    }
+    /** @type {("circle"|number)} */
+    const numSides = random(["circle", 4, 5, 6, 8]);
+    const numRings = random([1, 2, 3]);
+    repeat(numRings, (ix) => {
+        strokeWeight((random([1, 2, 3]) * cellSize) / 50);
+        const sz = cellSize / (ix + 1);
+        if (numSides === "circle") {
+            drawCircleRing(sz, palette);
+        } else {
+            drawShapeRing(numSides, sz, palette);
+        }
+    });
+}
+function drawCircleRing(cellSize, palette) {
+    push();
+    strokeWeight(random([1, 2, 3, 4, 5, 20]));
+    const diameter = (cellSize * 1) / random([1, 2, 3, 4]);
+    stroke(palette[4]);
+    noFill();
+    circle(0, 0, diameter);
+    pop();
+}
+/**
+ *
+ * @param {number} numSides
+ * @param {number} cellSize
+ * @param {string[]} palette
+ */
+function drawShapeRing(numSides, cellSize, palette) {
+    push();
+    rotate(PI / 2);
+    beginShape();
+    stroke(palette[0]);
+    const radius = cellSize / 2;
+    for (let angle = 0; angle < TWO_PI; angle += TWO_PI / numSides) {
+        const { x, y } = polarToCartesian(angle, radius);
+        vertex(x, y);
+    }
+    noFill();
+    endShape(CLOSE);
+    pop();
+}
+
+function polarToCartesian(angle, radius) {
+    const x = radius * cos(angle);
+    const y = radius * sin(angle);
+    return { x, y };
+}
+function repeat(numRepeats, fn) {
+    for (let i = 0; i < numRepeats; i++) {
+        fn(i);
     }
 }
